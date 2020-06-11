1324aa7c92adf55564f8864b465f0a00ac60c4b9
choryang <okee0815@gmail.com>
2020-06-12 01:06:01 +0900

    이미지 크기 조절 & 회전 조절

--- app/src/main/java/com/example/cosmingtest/CropActivity.java
+++ app/src/main/java/com/example/cosmingtest/CropActivity.java
@@ -1,24 +1,53 @@
 package com.example.cosmingtest;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CropActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    Button re_search;
+    Button go_result;
+    SearchActivity sa = (SearchActivity)SearchActivity.search_activity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crop);
 
-        ImageView imageView = (ImageView)findViewById(R.id.cr_image);
+        sa.finish();
+
+        imageView = findViewById(R.id.cr_image);
+        re_search = findViewById(R.id.re_search);
+        go_result = findViewById(R.id.go_result);
 
         byte[] arr = getIntent().getByteArrayExtra("image");
         Bitmap bm = BitmapFactory.decodeByteArray(arr, 0, arr.length);
 
         imageView.setImageBitmap(bm);
+
+        re_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent search_intent = new Intent(CropActivity.this, SearchActivity.class);
+                startActivity(search_intent);
+            }
+        });
+
+        go_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent result_intent = new Intent(CropActivity.this, ResultActivity.class);
+                startActivity(result_intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/cosmingtest/SearchActivity.java
+++ app/src/main/java/com/example/cosmingtest/SearchActivity.java
@@ -5,45 +5,79 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class SearchActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 672;
     private static final int REQUEST_IMAGE_GALLERY = 101;
+    public static Activity search_activity;
+    private String image_file_path;
+    private Uri photo_uri;
+    String photo_path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        search_activity = SearchActivity.this;
+
         //권한 체크
         TedPermission.with(getApplicationContext())
                 .setPermissionListener(permissionListener)
                 .setRationaleMessage("카메라 권한이 필요합니다.")
                 .setDeniedMessage("거부하셨습니다.")
-                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
                 .check();
 
 
         findViewById(R.id.camera).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE); //화면 전환이 일어났을 때 돌아오면서 값을 가지고 온다.*/
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE); //화면 전환이 일어났을 때 돌아오면서 값을 가지고 온다.
+                // Ensure that there's a camera activity to handle the intent
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photo_file = null;
+                    try {
+                        photo_file = createImageFile();
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                    }
+                    // Continue only if the File was successfully created
+                    if (photo_file != null) {
+                        photo_uri = FileProvider.getUriForFile(getApplicationContext(),
+                                getPackageName(),
+                                photo_file);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photo_uri);
+                        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
             }
         });
 
@@ -62,20 +96,56 @@ public class SearchActivity extends AppCompatActivity {
 
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "TEST_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        image_file_path = image.getAbsolutePath();
+        return image;
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         Bitmap bitmap = null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 5;
 
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK && data.hasExtra("data")) {
-            bitmap = (Bitmap) data.getExtras().get("data");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap temp_bitmap = BitmapFactory.decodeFile(image_file_path, options);
+            try {
+                ExifInterface exif = new ExifInterface(image_file_path);
+
+                int exifOrientation;
+                int exifDegree;
+
+                if (exif != null) {
+                    exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    exifDegree = exifOrientationToDegress(exifOrientation);
+                } else {
+                    exifDegree = 0;
+                }
+
+                bitmap = rotate(temp_bitmap, exifDegree);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         else if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == Activity.RESULT_OK) {
             try {
                 InputStream is = getContentResolver().openInputStream(data.getData());
-                bitmap = BitmapFactory.decodeStream(is);
+                bitmap = BitmapFactory.decodeStream(is, null, options);
                 is.close();
             }
             catch (Exception e) {
@@ -88,6 +158,7 @@ public class SearchActivity extends AppCompatActivity {
 
         if( bitmap != null ) {
             try {
+
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                 bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
                 byte[] byteArray = stream.toByteArray();
@@ -102,6 +173,26 @@ public class SearchActivity extends AppCompatActivity {
         else {
             Toast.makeText(getApplicationContext(), "이미지를 받아올 수 없습니다.", Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    private Bitmap rotate(Bitmap bitmap, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    private int exifOrientationToDegress(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        }
+        else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        }
+        else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
     }
 
 
