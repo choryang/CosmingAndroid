5135f5dbdd4a4aa44193e800ba43c7d982e04a3d
choryang <okee0815@gmail.com>
2020-06-13 20:30:23 +0900

    이미지 크기 조절

--- app/src/main/java/com/example/cosmingtest/SearchActivity.java
+++ app/src/main/java/com/example/cosmingtest/SearchActivity.java
@@ -8,6 +8,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
@@ -31,12 +33,15 @@ import java.util.Date;
 
 public class SearchActivity extends AppCompatActivity {
 
+    // 이미지 용량 조절
+    private final int REQUEST_WIDTH = 512;
+    private final int REQUEST_HEIGHT = 512;
     private static final int REQUEST_IMAGE_CAPTURE = 672;
     private static final int REQUEST_IMAGE_GALLERY = 101;
     public static Activity search_activity;
     private String image_file_path;
     private Uri photo_uri;
-    String photo_path;
+    CropActivity ca = (CropActivity)CropActivity.crop_activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +62,6 @@ public class SearchActivity extends AppCompatActivity {
         findViewById(R.id.camera).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE); //화면 전환이 일어났을 때 돌아오면서 값을 가지고 온다.*/
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 // Ensure that there's a camera activity to handle the intent
                 if (intent.resolveActivity(getPackageManager()) != null) {
@@ -85,11 +88,9 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(); //기기 기본 갤러리 접근
-                //갤러리 접근
                 intent.setType("image/jpeg");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(intent, REQUEST_IMAGE_GALLERY);
-
             }
         });
 
@@ -97,7 +98,7 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
-                overridePendingTransition(0, 0);
+                overridePendingTransition(0, 0);//화면전환 애니메이션 삭제
             }
         });
 
@@ -105,9 +106,9 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed() { // 뒤로가기 버튼 눌렀을 때
         super.onBackPressed();
-        overridePendingTransition(0, 0);
+        overridePendingTransition(0, 0);//화면 전환 애니메이션 삭제
     }
 
     private File createImageFile() throws IOException {
@@ -126,39 +127,73 @@ public class SearchActivity extends AppCompatActivity {
         return image;
     }
 
+    // 이미지 Resize 함수
+    private int setSimpleSize(BitmapFactory.Options options, int requestWidth, int requestHeight){
+        // 이미지 사이즈를 체크할 원본 이미지 가로/세로 사이즈를 임시 변수에 대입.
+        int originalWidth = options.outWidth;
+        int originalHeight = options.outHeight;
 
+        // 원본 이미지 비율인 1로 초기화
+        int size = 1;
+
+        // 해상도가 깨지지 않을만한 요구되는 사이즈까지 2의 배수의 값으로 원본 이미지를 나눈다.
+        while(requestWidth < originalWidth || requestHeight < originalHeight){
+            originalWidth = originalWidth / 2;
+            originalHeight = originalHeight / 2;
+
+            size = size * 2;
+        }
+        return size;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         Bitmap bitmap = null;
+        Bitmap temp_bitmap;
+        ExifInterface exif;
+        int exifOrientation;
+        int exifDegree;
         BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 4;
+        options.inJustDecodeBounds = true;
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bitmap temp_bitmap = BitmapFactory.decodeFile(image_file_path, options);
+            if (ca != null) {
+                ca.finish(); // 이전의 이미지 편집화면 종료
+            }
+            // inJustDecodeBounds = true일때
+            //bitmap은 반환하지않고, options 변수에만 값이 대입된다.
+            BitmapFactory.decodeFile(image_file_path, options);
+            options.inSampleSize = setSimpleSize(options, REQUEST_WIDTH, REQUEST_HEIGHT);
+            options.inJustDecodeBounds = false;
+            temp_bitmap = BitmapFactory.decodeFile(image_file_path, options);
             try {
-                ExifInterface exif = new ExifInterface(image_file_path);
-
-                int exifOrientation;
-                int exifDegree;
-
+                exif = new ExifInterface(image_file_path);
                 if (exif != null) {
                     exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-                    exifDegree = exifOrientationToDegress(exifOrientation);
+                    exifDegree = exifOrientationToDegrees(exifOrientation);
                 } else {
                     exifDegree = 0;
                 }
-
                 bitmap = rotate(temp_bitmap, exifDegree);
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+
         }
         else if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == Activity.RESULT_OK) {
+            if (ca != null) {
+                ca.finish(); // 이전의 이미지 편집화면 종료
+            }
             try {
                 InputStream is = getContentResolver().openInputStream(data.getData());
+                BitmapFactory.decodeStream(is, null, options);
+                options.inSampleSize = setSimpleSize(options, REQUEST_WIDTH, REQUEST_HEIGHT);
+                options.inJustDecodeBounds = false;
+                is = getContentResolver().openInputStream(data.getData());
                 bitmap = BitmapFactory.decodeStream(is, null, options);
                 is.close();
             }
@@ -172,7 +207,6 @@ public class SearchActivity extends AppCompatActivity {
 
         if( bitmap != null ) {
             try {
-
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                 bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
                 byte[] byte_array = stream.toByteArray();
@@ -196,7 +230,7 @@ public class SearchActivity extends AppCompatActivity {
         return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
     }
 
-    private int exifOrientationToDegress(int exifOrientation) {
+    private int exifOrientationToDegrees(int exifOrientation) {
         if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
             return 90;
         }
